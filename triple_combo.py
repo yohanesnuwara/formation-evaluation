518cec1d708562e99c76ad5179f97bf84f39ddf0
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-10-20 13:03:13 +0700

    Create triple_combo.py

---
+++ triple_combo.py
@@ -0,0 +1,106 @@
+"""
+Program to display Triple Combo
+
+@author: Yohanes Nuwara
+@email: ign.nuwara97@gmail.com
+"""
+
+def triple_combo(depth, GR, resistivity, NPHI, RHOB, 
+                 min_depth, max_depth, 
+                 min_GR=0, max_GR=150,
+                 min_resistivity=0.01, max_resistivity=1000, 
+                 color_GR='black', color_resistivity='green', 
+                 color_RHOB='red', color_NPHI='blue',
+                 figsize=(6,10), tight_layout=1, 
+                 title_size=15, title_height=1.05):
+  
+  import matplotlib.pyplot as plt
+  from matplotlib.ticker import AutoMinorLocator  
+
+  fig, ax=plt.subplots(1,3,figsize=(8,10))
+  fig.suptitle('Triple Combo Log', size=title_size, y=title_height)
+
+  ax[0].minorticks_on()
+  ax[0].grid(which='major', linestyle='-', linewidth='0.5', color='lime')
+  ax[0].grid(which='minor', linestyle=':', linewidth='1', color='black')
+
+  ax[1].minorticks_on()
+  ax[1].grid(which='major', linestyle='-', linewidth='0.5', color='lime')
+  ax[1].grid(which='minor', linestyle=':', linewidth='1', color='black')
+
+  ax[2].minorticks_on()
+  ax[2].grid(which='major', linestyle='-', linewidth='0.5', color='lime')
+  ax[2].grid(which='minor', linestyle=':', linewidth='1', color='black')  
+
+  # First track: GR
+  ax[0].get_xaxis().set_visible(False)
+  ax[0].invert_yaxis()   
+
+  gr=ax[0].twiny()
+  gr.set_xlim(min_GR,max_GR)
+  gr.set_xlabel('GR',color=color_GR)
+  gr.set_ylim(max_depth, min_depth)
+  gr.spines['top'].set_position(('outward',10))
+  gr.tick_params(axis='x',colors=color_GR)
+  gr.plot(GR, depth, color=color_GR)  
+
+  gr.minorticks_on()
+  gr.xaxis.grid(which='major', linestyle='-', linewidth='0.5', color='lime')
+  gr.xaxis.grid(which='minor', linestyle=':', linewidth='1', color='black') 
+
+  # Second track: Resistivity
+  ax[1].get_xaxis().set_visible(False)
+  ax[1].invert_yaxis()   
+
+  res=ax[1].twiny()
+  res.set_xlim(min_resistivity,max_resistivity)
+  res.set_xlabel('Resistivity',color=color_resistivity)
+  res.set_ylim(max_depth, min_depth)
+  res.spines['top'].set_position(('outward',10))
+  res.tick_params(axis='x',colors=color_resistivity)
+  res.semilogx(resistivity, depth, color=color_resistivity)    
+
+  res.minorticks_on()
+  res.xaxis.grid(which='major', linestyle='-', linewidth='0.5', color='lime')
+  res.xaxis.grid(which='minor', linestyle=':', linewidth='1', color='black')   
+
+  # Third track: NPHI and RHOB
+  ax[2].get_xaxis().set_visible(False)
+  ax[2].invert_yaxis()  
+
+  ## NPHI curve 
+  nphi=ax[2].twiny()
+  nphi.set_xlim(-0.15,0.45)
+  nphi.invert_xaxis()
+  nphi.set_xlabel('NPHI',color='blue')
+  nphi.set_ylim(max_depth, min_depth)
+  nphi.spines['top'].set_position(('outward',10))
+  nphi.tick_params(axis='x',colors='blue')
+  nphi.plot(NPHI, depth, color=color_NPHI)
+
+  nphi.minorticks_on()
+  nphi.xaxis.grid(which='major', linestyle='-', linewidth='0.5', color='lime')
+  nphi.xaxis.grid(which='minor', linestyle=':', linewidth='1', color='black')     
+
+  ## RHOB curve 
+  rhob=ax[2].twiny()
+  rhob.set_xlim(1.95,2.95)
+  rhob.set_xlabel('RHOB',color='red')
+  rhob.set_ylim(max_depth, min_depth)
+  rhob.spines['top'].set_position(('outward',50))
+  rhob.tick_params(axis='x',colors='red')
+  rhob.plot(RHOB, depth, color=color_RHOB)
+
+  # solution to produce fill between can be found here:
+  # https://stackoverflow.com/questions/57766457/how-to-plot-fill-betweenx-to-fill-the-area-between-y1-and-y2-with-different-scal
+  x2p, _ = (rhob.transData + nphi.transData.inverted()).transform(np.c_[RHOB,depth]).T
+  nphi.autoscale(False)
+  nphi.fill_betweenx(depth, NPHI, x2p, color="orange", alpha=0.4, where=(x2p>NPHI)) # hydrocarbon
+  nphi.fill_betweenx(depth, NPHI, x2p, color="blue", alpha=0.4, where=(x2p<NPHI)) # water
+
+  res.minorticks_on()
+  res.grid(which='major', linestyle='-', linewidth='0.5', color='lime')
+  res.grid(which='minor', linestyle=':', linewidth='1', color='black')
+
+  plt.tight_layout(tight_layout)  
+  plt.show() 
